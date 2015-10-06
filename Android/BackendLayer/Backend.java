8ee7875a79a28b8ebdb4aac0cc7a21bf6d32f67a
Michael Truell <truell20@gmail.com>
2015-10-06 09:11:20 -0400

    Started the Background Layer

---
+++ Android/BackendLayer/Backend.java
@@ -0,0 +1,12 @@
+public class Backend
+{
+    public static String getName(int userID) {
+        return "Michael";
+    }
+    
+    
+    public static String getEmail(int userID) {
+        return "michael_truell@horacemann.org";
+    }
+    
+}
