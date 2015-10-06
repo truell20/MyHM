7c149e26f45625c802a685fa3fe2d4b2588f7e20
Michael Truell <truell20@gmail.com>
2015-10-06 09:26:26 -0400

    Added Signin method

--- Android/BackendLayer/Backend.java
+++ Android/BackendLayer/Backend.java
@@ -1,5 +1,6 @@
 public class Backend
 {
+    // Gets the UserData of the user who has the userID given
     public static UserData getUserData(int userID) {
         UserData data = new UserData();
         data.name = "Michael Truell";
@@ -8,4 +9,19 @@ public class Backend
         
         return data;
     }
+    
+    /* Returns the userID the user whose email and password match the ones given.
+     * If there is no user with that combination of password and username,
+     * -1 is returned.
+     * This method may be used for signing a user in.
+     */
+    public static int getUserID(String email, String password) {
+        if(email == "michael_truell@horacemann.org" && password == "password") {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+    
+    
 }
