e64cc4a51129c4fab1798a473c223bbb49d92111
Michael Truell <truell20@gmail.com>
2015-10-07 10:25:49 -0400

    Removed Protection stuff

--- Backend/appositionapi.php
+++ Backend/appositionapi.php
@@ -7,22 +7,6 @@ class MyAPI extends API
 
     public function __construct($request, $origin) {
         parent::__construct($request);
-
-        // Abstracted out for example
-        $APIKey = new Models\APIKey();
-        $User = new Models\User();
-
-        if (!array_key_exists('apiKey', $this->request)) {
-            throw new Exception('No API Key provided');
-        } else if (!$APIKey->verifyKey($this->request['apiKey'], $origin)) {
-            throw new Exception('Invalid API Key');
-        } else if (array_key_exists('token', $this->request) &&
-             !$User->get('token', $this->request['token'])) {
-
-            throw new Exception('Invalid User Token');
-        }
-
-        $this->User = $User;
     }
 
     /**
@@ -30,7 +14,7 @@ class MyAPI extends API
      */
      protected function example() {
         if ($this->method == 'GET') {
-            return "Your name is " . $this->User->name;
+            return "Your name is Michael";
         } else {
             return "Only accepts GET requests";
         }
