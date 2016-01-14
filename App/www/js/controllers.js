c1d77cd871b137ab9dcd5130bb4766ab1bbdf510
joshuagruenstein <joshuagruenstein@gmail.com>
2016-01-13 23:04:25 -0500

    settings page

--- App/www/js/controllers.js
+++ App/www/js/controllers.js
@@ -4,4 +4,38 @@ angular.module('ApPosition.controllers', [])
 
 .controller('SearchCtrl', function($scope) {})
 
-.controller('SettingsCtrl', function($scope) {})
+.controller('SettingsCtrl', function($scope,$ionicPopup) {
+	$scope.data = {
+		showDelete: false
+	};
+
+	$scope.addFriend = function(friendName) {
+		$scope.friends.push({
+			name: friendName
+		});
+
+        $scope.friendName = "";
+    };
+
+	$scope.deleteFriend = function(friend) {
+		$scope.friends.splice($scope.friends.indexOf(friend), 1);
+	};
+
+	$scope.addFriendPrompt = function() {
+	   var promptPopup = $ionicPopup.prompt({
+	      title: 'Add Friend',
+	      template: "Enter your friend's name."
+	   });
+	   promptPopup.then(function(res) {
+	      if (res) {
+	         $scope.addFriend(res);
+	      }
+	   });
+	};
+
+	$scope.friends = [
+		{ name: 'Michael Truell' },
+		{ name: 'Luca Koval' },
+		{ name: 'Joshua Gruenstein' }
+	];
+})
