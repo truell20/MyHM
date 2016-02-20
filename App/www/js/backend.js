ebd6ac77c16ef4378bc4838aea425ad07588a62d
truell20 <truell20@gmail.com>
2016-02-19 22:22:08 -0500

    Namespaced backend

--- App/www/js/backend.js
+++ App/www/js/backend.js
@@ -1,41 +1,43 @@
-var url = "http://localhost:80/MyHM/Backend/";
+var backend = {
+	url: "http://localhost:80/MyHM/Backend/",
 
-function getUser(userID, email, password) {
-	return $http({
-		method: "GET",
-		url:url+"user",
-		data: {userID: userID, email: email, password: password}
-	});
-}
+	getUser: function(userID, email, password) {
+		return $http({
+			method: "GET",
+			url:url+"user",
+			data: {userID: userID, email: email, password: password}
+		});
+	},
 
-function searchForUser(searchTerm) {
-	return $http({
-		url: url+"user",
-		method: "GET",
-		data: {searchTerm: searchTerm}
-	});
-}
+	searchForUser: function(searchTerm) {
+		return $http({
+			url: url+"user",
+			method: "GET",
+			data: {searchTerm: searchTerm}
+		});
+	},
 
-function getSchedule(userID) {
-	return $http({
-		method: "GET",
-		url:url+"schedule",
-		data: {userID: userID}
-	});
-}
+	getSchedule: function(userID) {
+		return $http({
+			method: "GET",
+			url:url+"schedule",
+			data: {userID: userID}
+		});
+	},
 
-function getUserMeetings(userID) {
-	return $http({
-		url: url+"meeting",
-		method: "GET",
-		data: {userID: userID}
-	});
-}
+	getUserMeetings: function(userID) {
+		return $http({
+			url: url+"meeting",
+			method: "GET",
+			data: {userID: userID}
+		});
+	},
 
-function getMeeting(meetingID) {
-	return $http({
-		url: url+"meeting",
-		method: "GET",
-		data: {meetingID: meetingID}
-	});
+	getMeeting: function(meetingID) {
+		return $http({
+			url: url+"meeting",
+			method: "GET",
+			data: {meetingID: meetingID}
+		});
+	}
 }
